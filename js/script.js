d55a5a5bb1b0c76a54cfa846125f3acfda8e7045
Yelda Battı <yelda.batti@ogr.sakarya.edu.tr>
2025-12-04 23:11:33 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+[
+    {
+        "id":1, 
+        "title": "Proje 1: Adam Asmaca Oyunu",
+        "description": "Python programlama dilinde yapmış olduğum adam asmaca oyunu",
+        "image": "https://via.placeholder.com/300",
+        "link": "#"
+
+    }, 
+
+    {
+        "id":2,
+        "title":"Proje 2: Responsive Site",
+        "description": "HTML, CSS ve JAVASCRIPT Kullanarak yapmış olduğum resposive site örneği",
+        "image":"https://via.placeholder.com/300",
+        "link": "#"
+    },
+
+    {
+        "id":3,
+        "title":"Proje 3: Captcha App",
+        "description": "C# Programlama dilini kullanarak yapmış olduğum basit bir captcha uygulaması",
+        "image":"https://via.placeholder.com/300",
+        "link": "#"
+    }
+
+
+
+
+
+
+]
